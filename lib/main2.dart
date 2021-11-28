a1c59d093d8af29e5ea36ddcaf6aa5de291f4b59
lishu <lishugupta652@gmail.com>
2021-11-28 21:29:43 +0530

    routes

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
-import 'package:flutter_application/homepage.dart';
+import 'package:flutter_application/pages/Login.dart';
+import 'package:flutter_application/pages/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Homepage());
+    return MaterialApp(
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        routes: {
+          "/": (context) => LoginPage(),
+        });
   }
 }
---
+++ lib/pages/Login.dart
@@ -0,0 +1,16 @@
+// ignore: file_names
+import "package:flutter/material.dart";
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "LOGIN",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
